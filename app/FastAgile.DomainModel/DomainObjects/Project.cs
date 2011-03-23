1e496eaaa20354dce3c64524398b1d50a361c6fa
aph5 <netkmal@gmail.com>
2011-03-23 22:41:16 +0100

    added project snapshot

--- app/FastAgile.DomainModel/DomainObjects/Project.cs
+++ app/FastAgile.DomainModel/DomainObjects/Project.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using FastAgile.DomainModel.Events;
 using Ncqrs;
+using Ncqrs.Eventing.Sourcing.Snapshotting;
 
 namespace FastAgile.DomainModel.DomainObjects
 {
-    public class Project : Ncqrs.Domain.AggregateRootMappedByConvention
+    public class Project : Ncqrs.Domain.AggregateRootMappedByConvention, ISnapshotable<ProjectSnapshot>
     {
         private string _projectName;
         private string _projectDescription;
@@ -44,5 +45,38 @@ namespace FastAgile.DomainModel.DomainObjects
             _releases = new List<Release>();
             _users = new List<User>();
         }
+
+        public ProjectSnapshot CreateSnapshot()
+        {
+            return new ProjectSnapshot
+                       {
+                           CreationDate = _creationDate,
+                           ProjectBacklog = _projectBacklog,
+                           ProjectDescription = _projectDescription,
+                           ProjectName = _projectName,
+                           Releases = _releases,
+                           Users = _users
+                       };
+        }
+
+        public void RestoreFromSnapshot(ProjectSnapshot snapshot)
+        {
+            _projectName = snapshot.ProjectName;
+            _projectDescription = snapshot.ProjectDescription;
+            _projectBacklog = snapshot.ProjectBacklog;
+            _releases = snapshot.Releases;
+            _users = snapshot.Users;
+            _creationDate = snapshot.CreationDate;
+        }
+    }
+
+    public class ProjectSnapshot
+    {
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public Backlog ProjectBacklog { get; set; }
+        public List<Release> Releases { get; set; }
+        public List<User> Users { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
